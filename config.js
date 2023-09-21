7e62ca8566ce498b4cc76afb2485643aee5549f1
slsandarubot <127870206+slsandarubot@users.noreply.github.com>
2023-09-21 20:06:53 +0530

    Update config.js

--- config.js
+++ config.js
@@ -7,8 +7,8 @@ import axios from 'axios'
 
 
 global.owner = [
-  ['94740804536', 'Dinuwa Official²⁰²³', true],
-  ['94740804536', 'Dinuwa Official²⁰²³', true], 
+  ['94716159814', 'Sandaru Official²⁰²³', true],
+  ['94765295259', 'Sandaru Official²⁰²³', true], 
   [''] 
 ] //Number of owners
 
